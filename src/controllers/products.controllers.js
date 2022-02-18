28f6fd3ec6f6219814e33606f3a995b93b9cd58f
MARTIN.DOSSANTOS <martin.dossantos@naranja.com>
2022-02-17 21:36:03 -0300

    feat: cart

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -11,8 +11,10 @@ module.exports = {
     res.render(path.resolve(__dirname, '../views/products/product'));
   },
   cart: (req, res) => {
-    //return res.send('como vamos hasta aqui');
-    res.render(path.resolve(__dirname, '../views/products/cart'));
+    //aca hardcodeo user_1 pero deberia sustituirse por el id del usuario
+    //y cart.json sera sustituido por una entrada en la db o de ultima, en localstorage
+    const currentCart = require("../db/cart.json")["user_1"];    
+    res.render(path.resolve(__dirname, '../views/products/cart'),{products:currentCart});
   },
   admin: (req, res) => {
     //return res.send('como vamos hasta aqui');
