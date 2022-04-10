2275735e8ec63a5a6d321f2ea9ca7cdba8080850
Martin Ezequiel dos Santos <mds.designss@gmail.com>
2022-04-10 20:44:05 -0300

    Revert "Feature/add product views"

--- src/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/users.controllers.js');
-const logger = require('../middlewares/logger.js');
-
-router.get('/profile/:id',controller.profile);
-
-module.exports = router;
